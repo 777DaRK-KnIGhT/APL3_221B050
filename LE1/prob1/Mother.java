505e668a78eb866868e6db865ae43b0be747381d
221B050 <221B050@juet.co.in>
2025-01-27 14:56:43 +0530

    pushing folder LE1

---
+++ LE1/prob1/Mother.java
@@ -0,0 +1,13 @@
+class Mother {
+    int x = 10; 
+
+    
+    public Mother() {
+        System.out.println("Mother class constructor called");
+    }
+
+   
+    public void show() {
+        System.out.println("Mother's value of x: " + x);
+    }
+}
